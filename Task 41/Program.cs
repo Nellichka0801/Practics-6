de58ef12df694344177bf76cc63ad9cbb2b57e2c
Nelli <nellichka0801@yandex.ru>
2022-11-23 00:26:45 +1200

    Counter Of Positive Digits

---
+++ Task 41/Program.cs
@@ -0,0 +1,49 @@
+﻿// Пользователь вводит с клавиатуры M чисел. 
+//Посчитайте, сколько чисел больше 0 ввёл пользователь.
+//0, 7, 8, -2, -2 -> 2
+//1, -7, 567, 89, 223-> 3
+
+Console.WriteLine("Введите длину массива ");
+int length = int.Parse(Console.ReadLine()!);// длина массива
+
+int[] CreativeArrayFromInput(int length)
+{
+    int[] array = new int[length];
+    for (int i = 0; i < length; i++)
+     {
+        Console.WriteLine("Введите число массива и нажмите клавишу enter");
+        int numbers = int.Parse(Console.ReadLine()!);
+        array[i] = numbers;
+     }
+
+     return array;
+} 
+
+void Print(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i]);
+        if (i != array.Length - 1)
+        Console.Write(", ");
+    }
+
+Console.WriteLine();
+}
+        
+int CounterOfPositiveDigits (int[] array)
+{
+    int result=0;
+    for (int i = 0; i < length; i++)
+    {
+        if (array[i] > 0) result ++;
+    }
+    return result;
+}
+
+var array = CreativeArrayFromInput(length);
+Print(array);
+var count = CounterOfPositiveDigits (array);
+Console.WriteLine("Количество, введенных чисел больше нуля: " + count);
+System.Console.WriteLine();
+    
